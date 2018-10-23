ca78bbe590d91f76a175d572b4a67ee077c2452e
ferugi <me@ferugi.com>
2018-10-23 02:25:43 +0100

    Added basic TOML example to get started.

--- Presentations.Pidgin.Tests/ParseTomlTests.cs
+++ Presentations.Pidgin.Tests/ParseTomlTests.cs
@@ -1,10 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using Pidgin;
+using static Pidgin.Parser;
+using static Pidgin.Parser<char>;
+
+// Mention usings
 
 namespace Presentations.Pidgin.Tests
 {
-    class ParseTomlTests
+    public static class ParseTomlTests
+    {
+        public static string ParseTomlComment(string input)
+        {
+            // Build parser
+            Parser<char, char> hash = Char('#');
+            Parser<char, string> comment = hash
+                .Then(SkipWhitespaces.Then(AnyCharExcept('\r', '\n').ManyString()));
+
+            // Parse
+            var output = comment.ParseOrThrow(input);
+            return output;
+        }
+
+        [Fact]
+        public static void Toml_Comment_Parses_Correctly()
+        {
+            // Arrange
+            var stringToParse = "# This is a TOML document.\n";
+            var expectedComment = "This is a TOML document.";
+
+            // Act
+            var actualComment = ParseTomlComment(stringToParse);
+
+            // Assert
+            Assert.Equal(expectedComment, actualComment);
+        }
+    }
+
+    public interface IToml
     {
     }
 }
